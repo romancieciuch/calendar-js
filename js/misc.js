e4e5cb426cdeb453122479adf7e668811e61b41f
romancieciuch <roman@r1media.pl>
2026-04-14 14:37:20 +0200

    Menu mobilne

--- js/misc.js
+++ js/misc.js
@@ -4,11 +4,17 @@ export function misc () {
 		/* Menu mobilne */
 		const toggle = document.querySelector('.menu-toggle');
 		const nav = document.querySelector('.nav');
+		const links = document.querySelectorAll('.nav a');
 
 		toggle.addEventListener('click', () => {
 			nav.classList.toggle('open');
 			toggle.classList.toggle('active');
 		});
+
+		for (let el of links)
+			el.addEventListener("click", () => {
+				toggle.click();
+			});
 	}
 
 }
