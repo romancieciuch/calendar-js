7577dc920c295c8a5afac84c7383eafcf97b6d44
romancieciuch <roman@r1media.pl>
2026-04-14 10:18:43 +0200

    Widok tygodnia - wydarzenia już ładnie na siatce

--- views/week.js
+++ views/week.js
@@ -13,63 +13,95 @@ window.view = (params) => {
 	// Nawigacja
 	const navi = window.calendar.get_date_prev_next(week);
 
-	console.log(navi);
 
 	document.querySelector("[data-prev]").href = `#week/${navi.week.prev}`;
 	document.querySelector("[data-next]").href = `#week/${navi.week.next}`;
 
+	document.querySelector("[data-week-header]").innerHTML = render_week_header(week);
+	document.querySelector("[data-week-body]").innerHTML += render_week_events(days);
 
-	// <div class="week-day">
-	// 	<a
-	// 		class="week-event"
-	// 		href="#event/12345678"
-	// 		style="top: calc((9 - 8) * var(--hour-height)); height: calc(1 * var(--hour-height));"
-	// 	>9:00 Spotkanie</a>
-	// </div>
+	function render_week_events (week) {
+		let html = "";
 
+		week.days.forEach(day => {
+			let dayHTML = "";
 
-	// let html = ``;
-	// for (let el of days.days) {
+			let n = 0;
+			day.timed.forEach(e => {
+				const start = new Date(`${day.ymd}T${e.start}`);
+				const end = new Date(e.end);
 
-	// 	// Całodzienne
-	// 	let all_day = ``;
-	// 	for (let event of el.all_day) {
-	// 		let multiday_class = ``;
+				const startHour = start.getHours() + start.getMinutes() / 60;
+				const endHour = end.getHours() + end.getMinutes() / 60;
 
-	// 		if (event.is_multi_day) {
-	// 			if (event.is_first_day) multiday_class = ` month-event__multi_day month-event__first_day`;
-	// 			if (event.is_middle_day) multiday_class = ` month-event__multi_day month-event__middle_day`;
-	// 			if (event.is_last_day) multiday_class = ` month-event__multi_day month-event__last_day`;
-	// 		}
+				const duration = endHour - startHour;
 
-	// 		all_day += `
-	// 			<a class="month-event month-event__all_day${multiday_class}" href="#event/${event.id}" style="background: ${event.color}">
-	// 				${event.title}
-	// 			</a>
-	// 		`;
-	// 	}
+				// 🔥 pozycja względem siatki (zakładamy start od 6:00)
+				const top = `calc((${startHour} - 6) * var(--hour-height))`;
+				const left = `${n * (1 / day.timed.length) * 100}%`;
+				const width = `${(1 / day.timed.length) * 100}%`;
+				const height = `calc(${duration} * var(--hour-height))`;
 
-	// 	// Godzinowe
-	// 	let timed = ``;
-	// 	for (let event of el.timed)
-	// 		timed += `
-	// 			<a class="month-event month-event__timed" href="#event/${event.id}" style="border-color: ${event.color}">
-	// 				${event.start} &bull; ${event.title}
-	// 			</a>
-	// 		`;
+				const time =
+					String(start.getHours()).padStart(2, "0") +
+					":" +
+					String(start.getMinutes()).padStart(2, "0");
 
-	// 	html += `
-	// 		<div class="day ${el.type}-month ${el.is_today ? ' today' : ''}">
-	// 			<a href="#day/${el.ymd}" class="day-number">${el.date.getDate()}</a>
-	// 			${all_day}
-	// 			${timed}
-	// 		</div>
-	// 	`;
-	// }
+				dayHTML += `
+					<a
+						class="week-event"
+						href="#event/${e.id}"
+						style="top: ${top};
+							left: ${left};
+							width: ${width};
+							height: ${height};
+							background:${e.color};"
+					>
+						${time} ${e.title}
+					</a>
+				`;
 
-	// document.querySelector("[data-month]").innerHTML = html;
+				n++;
+			});
 
+			html += `<div class="week-day">${dayHTML}</div>`;
+		});
 
+		return html;
+	}
 
+	function render_week_header (date = new Date()) {
+		let d = new Date(date);
+		if (isNaN(d)) d = new Date(Date.now());
+
+		// znajdź poniedziałek
+		const day = d.getDay() || 7;
+		const monday = new Date(d);
+		monday.setDate(d.getDate() - day + 1);
+
+		const daysShort = ["Pon", "Wt", "Śr", "Czw", "Pt", "Sob", "Nd"];
+		const monthsShort = ["sty", "lut", "mar", "kwi", "maj", "cze", "lip", "sie", "wrz", "paź", "lis", "gru"];
+
+		let html = `<div></div>`; // puste pole (godziny)
+
+		for (let i = 0; i < 7; i++) {
+			const dayDate = new Date(monday);
+			dayDate.setDate(monday.getDate() + i);
+
+			const ymd = window.calendar.toYMD(dayDate);
+			const dayNum = dayDate.getDate();
+			const month = monthsShort[dayDate.getMonth()];
+
+			html += `
+				<div>
+					<a href="#day/${ymd}">
+						${daysShort[i]}, ${dayNum} ${month}
+					</a>
+				</div>
+			`;
+		}
+
+		return html;
+	}
 
 };
