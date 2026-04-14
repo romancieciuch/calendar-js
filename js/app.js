93686fc1893642fc03103154ee3f10def9b0e7c4
romancieciuch <roman@r1media.pl>
2026-04-14 14:47:22 +0200

    UX

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-import { routing }  from './routing.js';
-import { misc }     from './misc.js';
-import { Calendar } from './calendar.js';
+import { routing }  from './routing.js?v=1';
+import { misc }     from './misc.js?v=1';
+import { Calendar } from './calendar.js?v=1';
 
 window.calendar = new Calendar();
 
